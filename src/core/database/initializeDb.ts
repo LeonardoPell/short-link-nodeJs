d430a89101ed2608466cb93c7ee5b56dc1848eac
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-03-22 10:32:33 -0300

    feat: add initialize db

---
+++ src/core/database/initializeDb.ts
@@ -0,0 +1,26 @@
+import DataBaseConnection from './DataBaseConnection';
+
+async function initializeDb(){
+    const database = new DataBaseConnection();
+    await database.connect();
+    const client = database.getClient();
+    try {
+        // Executa a consulta para criar a tabela
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS links (
+                id VARCHAR(36) PRIMARY KEY,
+                code VARCHAR(255) NOT NULL,
+                url VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        console.log('Table "links" created successfully');
+    } catch (error) {
+        console.error('Error creating table:', error);
+    } finally {
+        await database.disconnect(); // Desconecta do banco de dados
+    }
+}
+
+initializeDb();
