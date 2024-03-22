f9155260cc981f307601793ed523008fecbdd88d
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-03-22 11:23:25 -0300

    feat: add debugger do catch return

--- src/core/exception/returnCatchExecptionController.ts
+++ src/core/exception/returnCatchExecptionController.ts
@@ -3,6 +3,11 @@ import ReplyResponse from "../replyResponse/ReplyResponse";
 import { FastifyReply } from "fastify/types/reply";
 
 export default function returnCatchExceptionController(error: any, reply: FastifyReply){
+
+    if(Boolean(process.env.DEBUGGER)){
+        console.log(error);
+    }
+
     if(
         error?.status &&
         error?.message
@@ -10,8 +15,6 @@ export default function returnCatchExceptionController(error: any, reply: Fastif
         const replyResponse = new ReplyResponse<null>(error.status, error.status, error.message, null);
         return reply.code(error.status).send(replyResponse);
     }
-
-    
 
     const replyResponse = new ReplyResponse<null>(httpStatus.INTERNAL_SERVER_ERROR, httpStatus[`${httpStatus.INTERNAL_SERVER_ERROR}_NAME`], 'Some unexpected error happened', null);
     return reply.code(httpStatus.INTERNAL_SERVER_ERROR).send(replyResponse);
