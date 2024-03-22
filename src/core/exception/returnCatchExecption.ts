86d31f2f016047a439d82bb9094e093eab243a46
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-03-22 00:38:29 -0300

    refactory: exception catchs functions

--- src/core/exception/returnCatchExecption.ts
+++ src/core/exception/returnCatchExecption.ts
@@ -1,16 +1,13 @@
-import httpStatus from "http-status";
-import ReplyResponse from "../replyResponse/ReplyResponse";
-import { FastifyReply } from "fastify/types/reply";
 
-export default function returnCatchException(error: any, reply: FastifyReply){
+import Exception from "./Exception";
+
+export default function returnCatchException(error: any){
     if(
         !error.status &&
         !error.message
     ){
-        const replyResponse = new ReplyResponse<null>(httpStatus.INTERNAL_SERVER_ERROR, httpStatus[`${httpStatus.INTERNAL_SERVER_ERROR}_NAME`], 'Some unexpected error happened', null);
-        return reply.code(httpStatus.INTERNAL_SERVER_ERROR).send(replyResponse);
+        throw new Error(error);
     }
-
-    const replyResponse = new ReplyResponse<null>(error.status, error.status, error.message, null);
-    return reply.code(error.status).send(replyResponse);
+    
+    throw new Exception(Number(error.status), String(error.message));
 }
---
+++ src/core/exception/returnCatchExecptionController.ts
@@ -0,0 +1,16 @@
+import httpStatus from "http-status";
+import ReplyResponse from "../replyResponse/ReplyResponse";
+import { FastifyReply } from "fastify/types/reply";
+
+export default function returnCatchExceptionController(error: any, reply: FastifyReply){
+    if(
+        !error.status &&
+        !error.message
+    ){
+        const replyResponse = new ReplyResponse<null>(httpStatus.INTERNAL_SERVER_ERROR, httpStatus[`${httpStatus.INTERNAL_SERVER_ERROR}_NAME`], 'Some unexpected error happened', null);
+        return reply.code(httpStatus.INTERNAL_SERVER_ERROR).send(replyResponse);
+    }
+
+    const replyResponse = new ReplyResponse<null>(error.status, error.status, error.message, null);
+    return reply.code(error.status).send(replyResponse);
+}
